5260f2119d98572efe7b6cfd47e62f50e7543609
CryptonFuture <crypton.futuremedia1989@gmail.com>
2023-02-08 17:49:19 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const multer = require('multer')
+
+const port = process.env.PORT || 4000
+
+const upload = multer({dest: 'uploads/'}).single("demo_image")
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Server is working")
+})
+
+app.post("/image", (req, res) => {
+    upload(req, res, (err) => {
+        if(err) {
+            res.status(400).send("Something went wrong")
+        }
+        res.send(req.file)
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is localhost on Port ${port}`)
+})
